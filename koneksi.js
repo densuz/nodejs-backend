8ccba1a531806082730465b0fa3b5870e9fec638
densuz <densuz97@gmail.com>
2020-07-14 01:20:50 +0700

    create files routes,koneksi,auth,index & folder config,middleware

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+//Membuat koneksi  ke database
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_gulajahe'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+});
+
+module.exports = conn;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
+const app = express();
+var cors = require ('cors');
+
+//parse application/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
+
+// Port 1997
+app.listen(1997, () => {
+    console.log('Server started on port 3001');
+});
